60d89a5b55f6cc57429e43acbbc806dcbfa6a320
Mayco Dominguez <maycodominguez123@gmail.com>
2024-04-14 02:08:59 -0300

    almost

--- favorites/favoritas.js
+++ favorites/favoritas.js
@@ -6,7 +6,7 @@ fetch('https://moviestack.onrender.com/api/movies', {
     .then(res => res.json())
     .then(data => {
         const peliculas = data.movies; // Obtener las películas de la API
-        const favoritos = localStorage.getItem('favoritos') ? JSON.parse(localStorage.getItem('favoritos')): [] // Obtener los IDs de las películas favoritas del localStorage
+        const favoritos = localStorage.getItem('favoritos') ? JSON.parse(localStorage.getItem('favoritos')) : [] // Obtener los IDs de las películas favoritas del localStorage
 
         // Filtrar las películas favoritas
         const peliculasFavoritas = peliculas.filter(pelicula => favoritos.includes(pelicula.id));
@@ -14,21 +14,27 @@ fetch('https://moviestack.onrender.com/api/movies', {
         // Renderizar las películas favoritas en la página
         const favoritosContainer = document.getElementById('favoritosContainer');
         favoritosContainer.innerHTML = ''; // Limpiar el contenedor antes de renderizar
+        if(peliculasFavoritas.length === 0){
+            favoritosContainer.innerHTML = '<p class= "text-xl italic">"No hay nada en favoritos"</p>'
+        }
+        else{
 
-        peliculasFavoritas.forEach(pelicula => {
-            // Generar el HTML para cada película favorita
-            const peliculaHTML = `
-            <div class="pelicula-favorita p-4 text-center flex justify-center items-center flex-col border border-black w-full sm:w-1/2 md:w-1/3 lg:w-1/3">
-        <img src="https://moviestack.onrender.com/static/${pelicula.image}" alt="${pelicula.title}" class="pelicula-imagen">
-        <div class="pelicula-info text-white italic">
-            <h2 class="pelicula-titulo text-white">${pelicula.title}</h2>
-            <p class="pelicula-resumen text-white">${pelicula.overview}</p>
-            <a href="${pelicula.homepage}" class="pelicula-enlace text-gray-500 hover:text-blue-700 text-xl font-bold uppercase mt-2 inline-block hover:underline">Más información</a>
-        </div>
-    </div>
-        
-        `;
-            favoritosContainer.innerHTML += peliculaHTML;
-        });
-    })
+            peliculasFavoritas.forEach(pelicula => {
+                // Generar el HTML para cada película favorita
+                const peliculaHTML = `
+                <div class="pelicula-favorita p-4 text-center flex justify-center items-center flex-col border border-black w-full sm:w-1/2 md:w-1/3 lg:w-1/3">
+                <img src="https://moviestack.onrender.com/static/${pelicula.image}" alt="${pelicula.title}" class="pelicula-imagen">
+                <div class="pelicula-info text-white italic">
+                <h2 class="pelicula-titulo text-white">${pelicula.title}</h2>
+                <p class="pelicula-resumen text-white">${pelicula.overview}</p>
+                <a href="${pelicula.homepage}" class="pelicula-enlace text-gray-500 hover:text-blue-700 text-xl font-bold uppercase mt-2 inline-block hover:underline">Más información</a>
+                </div>
+                </div>
+                
+                `;
+                favoritosContainer.innerHTML += peliculaHTML;
+            });
+        }
+    }
+    )
     .catch(err => console.log(err));
